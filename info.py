9430c3ebeea822b18022e8d33931775bd4d2c6bb
Speedwolf02 <158140629+Speedwolf02@users.noreply.github.com>
2025-10-02 08:17:28 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,7 @@
+from os import environ
+
+API = environ.get("API", "35bb655ec06018f724ea4f3cde2b410b2f34c646") # shortlink api
+URL = environ.get("URL", "adrinolinks.in") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+Gt55OVP7VTAyNmNl") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "Ggjkkkooo_bot") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
