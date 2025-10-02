2604a7c476f1e5549465fe732a9a509a46e72ad4
Speedwolf02 <158140629+Speedwolf02@users.noreply.github.com>
2025-10-02 12:10:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,11 +4,14 @@ from os import environ
 API_ID =  24435985
 API_HASH = "0fec896446625478537e43906a4829f8"
 BOT_TOKEN = "7758738938:AAFfwe6FlwIU22cwDdzhqWqlLSeJo9V1p_Q"
+ADMIN=[6299192020]
+DEL_TIME= 1 # Enter time in hours for token expiry 
+
 
 #url shorter website data
 
-API = environ.get("API", "f751ce50e4af7ba0fada8796cd85efbd61992ed2") # shortlink api
-URL = environ.get("URL", "earnpays.com") # shortlink domain without https://
+API = environ.get("API", "35bb655ec06018f724ea4f3cde2b410b2f34c646") # shortlink api
+URL = environ.get("URL", "adrinolinks.com") # shortlink domain without https://
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+Gt55OVP7VTAyNmNl") # how to open link 
 BOT_USERNAME = environ.get("BOT_USERNAME", "Ghjjjoooo_bot") # bot username without @
 VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
